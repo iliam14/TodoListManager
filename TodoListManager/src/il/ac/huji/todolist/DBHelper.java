e3dd48d4c8c0bbfed22a30b505361b7b2f29ca5f
iliam14 <ilia.marin@mail.huji.ac.il>
2014-04-26 17:26:38 +0300

    Removed usage of parse

--- TodoListManager/src/il/ac/huji/todolist/DBHelper.java
+++ TodoListManager/src/il/ac/huji/todolist/DBHelper.java
@@ -1,21 +1,11 @@
 package il.ac.huji.todolist;
 
 import java.util.Date;
-import java.util.List;
-
-import com.parse.FindCallback;
-import com.parse.Parse;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -25,8 +15,6 @@ public class DBHelper extends SQLiteOpenHelper {
 	private static final String COLUMN_ID = "_id";
 	private static final String COLUMN_TITLE = "title";
 	private static final String COLUMN_DUE = "due";
-	private static final String COLUMN_SYNC = "LocalId";
-	private static final String PARSE_CLASS = "todo";
 
 	private static final String CREATE_DB = "create table " + TABLE_NAME + "("
 			+ COLUMN_ID + " integer primary key autoincrement, " + COLUMN_TITLE
@@ -36,8 +24,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
 	private DBHelper(Context context) {
 		super(context, DB_NAME, null, 1);
-		Parse.initialize(context, "8iZEZzycf1oE2yMHUMYYBccZ1QBBU4ch00bpNXJt",
-				"VMg7Icyf2phMhUd7ZzW4I7kfyN1qvyNrIT0rJVCs");
+	
 
 	}
 
@@ -51,24 +38,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		ParseQuery<ParseObject> query = ParseQuery.getQuery(PARSE_CLASS);
-
-		// Clear the entries in the Parse Table
-		query.findInBackground(new FindCallback<ParseObject>() {
-			@Override
-			public void done(List<ParseObject> resList, ParseException e) {
-				if (e == null) {
-					for (ParseObject object : resList) {
-						object.deleteInBackground();
-					}
-				} else {
-					Log.d("DBHelper", "Error: " + e.getMessage());
-				}
-
-			}
-		});
-
-		ParseUser.enableAutomaticUser();
+	
 		db.execSQL(CREATE_DB);
 
 	}
@@ -83,15 +53,9 @@ public class DBHelper extends SQLiteOpenHelper {
 		ContentValues task = new ContentValues();
 		task.put(COLUMN_TITLE, taskName);
 		task.put(COLUMN_DUE, due.getTime());
-		long KeyID = getWritableDatabase().insert(TABLE_NAME, null, task);
+		getWritableDatabase().insert(TABLE_NAME, null, task);
 
-		// Inset task to parse
-		ParseObject parseTask = new ParseObject(PARSE_CLASS);
-		parseTask.put(COLUMN_TITLE, taskName);
-		parseTask.put(COLUMN_DUE, due.getTime());
-		parseTask.put(COLUMN_SYNC, String.valueOf(KeyID));
-		parseTask.saveInBackground();
-
+	
 	}
 
 	public void remove(int id) {
@@ -99,27 +63,7 @@ public class DBHelper extends SQLiteOpenHelper {
 		getWritableDatabase().delete(TABLE_NAME, "_id = ?",
 				new String[] { String.valueOf(id) });
 
-		// remove from Parse
-		ParseQuery<ParseObject> query = ParseQuery.getQuery(PARSE_CLASS);
-		query.whereEqualTo(COLUMN_SYNC, String.valueOf(id));
-		query.findInBackground(new FindCallback<ParseObject>() {
-
-			@Override
-			public void done(List<ParseObject> resList, ParseException e) {
-				if (e == null) {
-					if (resList.size() > 1)
-						Log.d("DBHelper",
-								"get List of length: " + resList.size());
-					else {
-						resList.get(0).deleteInBackground();
-					}
-				} else {
-					Log.d("DBHelper", "Error: " + e.getMessage());
-				}
-
-			}
-		});
-	}
+		}
 
 	public Cursor getCursor() {
 		return getWritableDatabase().query(TABLE_NAME,
